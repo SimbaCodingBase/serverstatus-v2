dd340f620ef06f5fe139be0b9445a42fab22dfbf
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2022-01-09 03:50:54 +0100

    Update db_connecter.php

--- _ntl/db_connecter.php
+++ _ntl/db_connecter.php
@@ -1,1 +1,25 @@
-/
+<?php
+error_reporting(E_ALL);
+
+// Zum Aufbau der Verbindung zur Datenbank
+define ( 'MYSQL_HOST',      'localhost' );
+define ( 'MYSQL_BENUTZER',  'serverstatusv2' );
+define ( 'MYSQL_KENNWORT',  '86@wj7zK' );
+define ( 'MYSQL_DATENBANK', 'serverstatusv2' );
+
+$db_link = mysqli_connect (MYSQL_HOST, 
+                           MYSQL_BENUTZER, 
+                           MYSQL_KENNWORT, 
+                           MYSQL_DATENBANK);
+
+if ( $db_link )
+{
+    echo 'Verbindung erfolgreich: ';
+    print_r( $db_link);
+}
+else
+{
+    die('keine Verbindung möglich: ' . mysqli_error());
+}
+?>
+
